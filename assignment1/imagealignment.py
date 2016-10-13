ee88af5fb178d32ac25e676ef201155420cb8bd3
Abhinav Gupta <abhinavgupta94@gmail.com>
2016-10-13 18:08:12 -0400

    correct image alignment

--- assignment1/imagealignment.py
+++ assignment1/imagealignment.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import distance
+import scipy
 import random
 from PIL import Image
 
@@ -43,27 +44,25 @@ for i in range(N):
     for j in range(3):
         X.append([src_pts[points[j]][0], src_pts[points[j]][1], 0, 0, 1, 0])
         X.append([0, 0, src_pts[points[j]][0], src_pts[points[j]][1], 0, 1])
-        X_dash.append(dst_pts[points[j]][0])
-        X_dash.append(dst_pts[points[j]][1])
-
-    X = np.array(X)
-    X_dash = np.array(X_dash)
+        X_dash.append([dst_pts[points[j]][0]])
+        X_dash.append([dst_pts[points[j]][1]])
+    X = np.matrix(X)
+    X_dash = np.matrix(X_dash)
     try:
-        np.linalg.solve(X, X_dash)
+        A = np.linalg.solve(X, X_dash)
     except np.linalg.linalg.LinAlgError as err:
         if 'Singular matrix' in err.message:
             continue
-    A = np.linalg.solve(X, X_dash)
-    # print A
-
-    slope = [[A[0], A[1]], [A[2], A[3]]]
-    intercept = [A[4], A[5]]
 
     temp = []
     count = 0
     for l in range(len(src_pts)):
-        transformedPoint = np.dot(slope, src_pts[l]) + intercept
-        originalPoint = dst_pts[l]
+        X = []
+        X.append([src_pts[l][0], src_pts[l][1], 0, 0, 1, 0])
+        X.append([0, 0, src_pts[l][0], src_pts[l][1], 0, 1])
+        # X = np.matrix(X)
+        transformedPoint = np.dot(X,A)
+        originalPoint = np.matrix([dst_pts[l][0], dst_pts[l][1]])
         if distance.euclidean(originalPoint, transformedPoint) < 10:
             count += 1
             temp.append(l)
@@ -71,15 +70,29 @@ for i in range(N):
     if count > maxCount:
         maxCount = count
         inliers = temp[:]
-        bestSlope = slope[:]
-        bestIntercept = intercept[:]
+        bestA = A[:]
 
 # print maxCount
 
 transformedInliers = []
+X = []
+X_dash = []
 for i in range(len(inliers)):
-    transformedPoint = np.dot(bestSlope, src_pts[inliers[i]]) + bestIntercept
-    transformedInliers.extend((transformedPoint[0], transformedPoint[1]))
+    X.append([src_pts[inliers[j]][0], src_pts[inliers[j]][1], 0, 0, 1, 0])
+    X.append([0, 0, src_pts[inliers[j]][0], src_pts[inliers[j]][1], 0, 1])
+    X_dash.append([dst_pts[inliers[j]][0]])
+    X_dash.append([dst_pts[inliers[j]][1]])
+
+    # transformedPoint = np.dot(X,bestA)
+    # transformedInliers.extend((transformedPoint[0], transformedPoint[1]))
+
+X = np.matrix(X)
+X_dash = np.matrix(X_dash)
+
+# X_T = np.transpose(X)
+# A = np.dot(np.dot(np.dot(X_T,X),X_T),X_dash)
+# A = np.linalg.solve(X, X_dash)
+# print bestA
 
 images = map(Image.open, ['sift_img1.jpg', 'sift_img2.jpg'])
 widths, heights = zip(*(i.size for i in images))
@@ -101,10 +114,16 @@ for i in range(len(inliers)):
 
 cv2.imwrite("transformed.jpg", combinedImage)
 
-H = slope[:]
-for sl in range(len(H)):
-    H[sl].append(intercept[sl])
-H = np.array(H)
-final = cv2.warpAffine(img1, H, (img1.shape[1],img1.shape[0]))
+# pts = np.float32([kp1[m].pt for m in range(len(kp1))])
 
+bestA = np.array(bestA).ravel()
+H = []
+H.append([bestA[0], bestA[1], bestA[4]])
+H.append([bestA[2], bestA[3], bestA[5]])
+H = np.matrix(H)
+print H
+final = cv2.warpAffine(img1, H, (img1.shape[1],img1.shape[0]))
+# plt.subplot(121),plt.imshow(img1),plt.title('Input')
+# plt.subplot(122),plt.imshow(final),plt.title('Output')
+# plt.show()
 cv2.imwrite("final.jpg", final)
