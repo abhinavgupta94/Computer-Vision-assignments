e65e3b29892062df91ef1c49fa1816ecf9a4208a
Abhinav Gupta <abhinavgupta94@gmail.com>
2016-10-16 18:42:11 -0400

    final commit

--- assignment-1/master_a1.sh
+++ assignment-1/master_a1.sh
@@ -1,4 +1,7 @@
-#! /bin/bash
+#!/bin/bash
+
+#Version Info: Python 2.7.12 :: Anaconda 4.2.0 (x86_64)
+#Packages used: opencv=2.4.8, numpy=1.11.2, scipy=0.18.1, matplotlib=1.5.3
 
 python gaussianfilter.py
 python imagealignment.py
